e3a2918a34feaac73401afda5777f80ce28e52e5
Aditya Shah <shahaditya@outlook.com>
2024-01-23 19:37:33 -0800

    Flask server created

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from github import Github
 import pptx
 from pptx.util import Pt, Inches
+from flask import Flask, render_template
 
+
+
+app = Flask(__name__)
 
 # auth = Auth.Token("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")
 g = Github()
@@ -20,37 +24,26 @@ print(issues)
 for issue in issues:
     print (issue.title)
 
+# left = top = width = height = Inches(1)
+# txBox = slide.shapes.add_textbox(Inches(3.43), Inches(1.86), Inches(2.02), Inches(0.91))
+# tf = txBox.text_frame
+# temp = tf.add_paragraph()
+# temp.text = issues[2].title
+# temp.font.size = Pt(20)
 
-# for shape in slide.shapes:
-#     if shape.has_text_frame:
-#         text_frame = shape.text_frame
-#         for paragraph in text_frame.paragraphs:
-#             for run in paragraph.runs:
-#                 text = run.text
-#                 if "Task" in text:
-#                     new_text = text.replace("Task", "new Task")
-#                     run.text = new_text
-
-left = top = width = height = Inches(1)
-txBox = slide.shapes.add_textbox(Inches(3.43), Inches(1.86), Inches(2.02), Inches(0.91))
-tf = txBox.text_frame
-temp = tf.add_paragraph()
-temp.text = issues[2].title
-temp.font.size = Pt(20)
-
-try:
-    p = tf.add_paragraph()
-    p.text = issues[2].assignee
-    p.font.size = Pt(30)
-    prs.save("public/newSlide.pptx")
-except:
-    prs.save("public/newSlide.pptx")
+# try:
+#     p = tf.add_paragraph()
+#     p.text = issues[2].assignee
+#     p.font.size = Pt(30)
+#     prs.save("public/newSlide.pptx")
+# except:
+#     prs.save("public/newSlide.pptx")
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# slide.placeholders[1].text = issues[0].title
-# print("thing, ", slide.placeholders[0].text)
-#prs.save("public/newSlide.pptx")
 
-
-    
+if __name__ == '__main__':
+    app.run()
